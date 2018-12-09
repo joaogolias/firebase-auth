6b853c8a0cbc3e18f055d17404ca22427f0568b4
Joao Golias <golias.appsimples@gmail.com>
2018-12-09 14:26:11 -0200

    updated gitignore

--- src/config.ts
+++ src/config.ts
@@ -11,6 +11,20 @@ export interface Config {
 
 
 export const getConfig = (): Config => {
-    return {} as Config
+    return {
+        firebase: {
+            type: "service_account",
+            projectId: "fir-auth-68f98",
+            privateKeyId: "196726f45cbe7b301541060a2559aa29e0bd9b60",
+            privateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDXxA29HDUk0I+w\nZqvKi4Ox2ENp8vzsWNe54obx26T/ZV5/i+dz3aOEgUK9ban41ui13nnS/bWHq6aq\nxaVUQ9HVhfpwfqpeb1vMU5pYkYuQRMpIshoIbv9dFXxmyf8OFzxPvMtQDCXMHOir\nejhSHYIk3CsNNGiF+I9GqXWOh8Xw1iwZIBAab6LAjIJfCLibYDug0JxtlMAkh0wT\nYX2tczr9/64xOGvLrtFQyavJzBK5BcYIEvPazcF8qIMiqgoV9wVWkMd84ZJ39TYM\n8dhM4/kcWV2lv6YL5jXjLz1naRGlpj+jmlVGkTaM9ZHr8XfdrvYSFnriqk6M49RW\n7+7BlyLHAgMBAAECggEAAqM9wcE9sGiw9lNC/V5i6pfTYdEGdi3d2+1g1fBn83ao\nnD7jF5+u1zispAaFAerqpwfZuGfBQi/UgUOaR6ND8OwgNCOFmf2a2IQbH8MOZYyZ\nu8rFll3OVwkjX1mlJGXJ/HWOjLjL9iinpX+rKZcnwfiZDs0Ztq2OXtWaf3a5x5MT\nl3aAvpNxaU3YcpR0SKH7iY69Lrw1cPSGFi2bTCc+nP0XiEUtCFiLiVu/g4ORjuMj\nr7HUEkJA40HgAchHmYEG18Reau7TX3u3hBUrs2S2eu0ekvR+1zsJakuL+NEI5Il5\njOfhZKElksoAXcnxpQidCUxNj6khAMZ/sp/RyZhoPQKBgQD+thsETun7hZPASf7R\nOQGG4iRO+Wn9NcMsUzSBzCBmVQyWkyVPEjQElvM2YKQ/fq46ayRZ/sSQwpupY0iQ\nRULhJxeMW3OtSYHUyghLFCM1+imrgTE5AZTIun0E2TKcOCH+ma+k4j485wBIaS4Y\nf3aHXZp6VPGx9pkyMkRvdyJ9QwKBgQDY24HPpOezdBYTKUEDh1nOMC3ZoYW4EcQp\n9un+/hF0PvVgJHqLYgJR17rOeh4prQt5Bsyy3J/baGDo+07tZVpmtuNqwFHWmOFM\nxMvneDVF1O8LG/XBAWkGe4VY+ohaMPQ5CQiTk9DA5fKTH7cJ2LuGieDA7LjFuyTU\nlxxfymuKLQKBgDTkSd3uKSZiyZivhOubY9bHlPSVVE4RYPMOYCPn6H53gMKv0ZqG\n1Chz1BNhpBnPS4yz/Pt3LQhGcR6OkF3tFAM/I+OwhefhEMXIIkq85sbpqMeFb1xA\nGM5J83YTSDAqc9yzu0aI+JWa/nMK7FiH92wKou5uPzZOV+eViqJ+4bmTAoGAISAu\nzSQ4jI4QDS9BEiPr0PZFGrh0r7BvEaX5Co2O5v3zB5Oq8H2xMwVMMNwwnVckqIEk\nZbIT7TUdkJlK7jKMQ6JuTNFB7xD9YXeyWhJfuOIU/dJwkOIBRA88SGJEz9oohEnf\n+IMc4Up+6oBuul9UDfIj1fSTKQk0UBEhMXS/RqUCgYEA3UECE4Np31uQSE9n8qut\nwxPjgq/q7JyXMzzSF12ij1db8QWO4jzQ1mg8MsS0Xxhka0U5eNpefC2C7Zk/i3kd\nFds/iZld2k32V7wcO1xjDD7158TrAw5S2Hg7tOxG3UF0TeGmtZ6hrprElhojUkCO\nrxljnuEHaBHXu5rKAov0Wko=\n-----END PRIVATE KEY-----\n",
+            clientEmail: "firebase-adminsdk-r4xs7@fir-auth-68f98.iam.gserviceaccount.com",
+            clientId: "115660427603388343265",
+            authUri: "https://accounts.google.com/o/oauth2/auth",
+            tokenUri: "https://oauth2.googleapis.com/token",
+            authProviderX509CertUrl: "https://www.googleapis.com/oauth2/v1/certs",
+            clientX509CertUrl: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-r4xs7%40fir-auth-68f98.iam.gserviceaccount.com",
+            apiKey: "AIzaSyD4l8Ze6AWTVeJnUk4PUY7myZK66ZrAZ70",
+        }
+          
+    }
 }
-
